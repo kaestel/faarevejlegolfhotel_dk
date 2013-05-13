c0dfe6f7d11fe11043050aec3b4f3b936a38adf0
kaestel <martin@hvadhedderde.com>
2013-05-13 18:20:21 +0200

    Added license

---
+++ src/www/legal/index.php
@@ -0,0 +1,12 @@
+<?php
+	$body_class = "legal";
+	$page_title = "Legal stuff";
+	include($_SERVER["LOCAL_PATH"]."/templates/shell_header.php");
+?>
+
+	<div class="box">
+		<h1>Legal stuff</h1>
+
+	</div>
+
+<?php include($_SERVER["LOCAL_PATH"]."/templates/shell_footer.php") ?>
